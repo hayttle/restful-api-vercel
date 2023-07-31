4aca77f73edb7e57b2787ef4c49760fb24aacced
hayttle <hayttle@gmail.com>
2023-07-30 21:04:28 -0300

    fix: enable cors vercel

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // JSON Server module
 const jsonServer = require("json-server")
+const cors = require("cors")
 const server = jsonServer.create()
 const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults()
 
+server.use(cors())
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(
